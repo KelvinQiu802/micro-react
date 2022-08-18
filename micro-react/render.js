8ad67d584a692c4934cca4e6492a8c5a1c166b24
KelvinQiu <kelvinqiu802@outlook.com>
2022-08-18 22:47:59 +0800

    feat: Fibers

--- micro-react/render.js
+++ micro-react/render.js
@@ -1,20 +1,28 @@
-function render(element, container) {
+function createDOM(fiber) {
   // 创建父节点
   const dom =
-    element.type == 'TEXT_ELEMENT'
+    fiber.type == 'TEXT_ELEMENT'
       ? document.createTextNode('')
-      : document.createElement(element.type);
+      : document.createElement(fiber.type);
 
   // 赋值属性
-  Object.keys(element.props)
+  Object.keys(fiber.props)
     .filter((key) => key !== 'children')
-    .forEach((key) => (dom[key] = element.props[key]));
+    .forEach((key) => (dom[key] = fiber.props[key]));
 
-  // 递归，渲染每一个child
-  element.props.children.forEach((child) => render(child, dom));
+  return dom;
+}
 
-  // 将子节点添加到父节点下
-  container.append(dom);
+// 开始渲染
+function render(element, container) {
+  // Root Fiber
+  nextUnitOfWork = {
+    dom: container,
+    props: {
+      children: [element],
+    },
+    child: null,
+  };
 }
 
 let nextUnitOfWork = null;
@@ -36,6 +44,54 @@ function workLoop(deadline) {
 requestIdleCallback(workLoop);
 
 // 执行一个渲染任务单元，并返回新的任务
-function performUnitOfWork(nextUnitOfWork) {}
+function performUnitOfWork(fiber) {
+  // 新建DOM元素
+  if (!fiber.dom) {
+    fiber.dom = createDOM(fiber);
+  }
+
+  // 渲染DOM
+  if (fiber.parent) {
+    fiber.parent.dom.append(fiber.dom);
+  }
+
+  // 给children创建fiber
+  const elements = fiber.props.children;
+  let prevSibling = null;
+  for (let i = 0; i < elements.length; i++) {
+    const newFiber = {
+      type: elements[i].type,
+      props: elements[i].props,
+      parent: fiber,
+      child: null,
+      sibling: null,
+      dom: null,
+    };
+
+    // 第一个child才可以作为child，其他的就是sibling
+    if (i === 0) {
+      fiber.child = newFiber;
+    } else {
+      prevSibling.sibling = newFiber;
+    }
+    prevSibling = newFiber;
+  }
+
+  // 如果有child，就返回child fiber
+  if (fiber.child) {
+    return fiber.child;
+  }
+  // 没有就优先返回兄弟，向上查找
+  // 如果没有，就不返回，返回值为undefined
+  let nextFiber = fiber;
+  while (nextFiber) {
+    // 有sibling
+    if (nextFiber.sibling) {
+      return nextFiber.sibling;
+    }
+    // 向上查找
+    nextFiber = nextFiber.parent;
+  }
+}
 
 export default render;
